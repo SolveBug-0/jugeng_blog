40b17293209453a60a114b25b084b6a0d9fce676
SolveBug-0 <1875708586@qq.com>
2022-08-12 18:00:36 +0800

    第一次提交

---
+++ blog-api/src/main/java/com/solvebug/blogapi/BlogApp.java
@@ -0,0 +1,14 @@
+package com.solvebug.blogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApp.class,args);
+    }
+
+}
